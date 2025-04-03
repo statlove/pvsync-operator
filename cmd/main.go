ab808f17c7bb1540d234a03330b528ceed464f83
statlove <juniyo97@naver.com>
2025-04-03 09:31:19 +0000

    Fix: modify new main.go and resolve build error

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
